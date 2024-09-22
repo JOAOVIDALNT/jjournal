7159e435f690f2afcf41dc82d961193a0c0b02e3
João Vidal <dev.joaovidal@gmail.com>
2024-09-21 22:52:49 -0300

    fix: internal resource file problem by setting ResXFileCodeGenerator at properties

--- backend/shared/jjournal.Exception/ResourceMessageException.Designer.cs
+++ backend/shared/jjournal.Exception/ResourceMessageException.Designer.cs
@@ -22,7 +22,7 @@ namespace jjournal.Exception {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class ResourceMessageException {
+    public class ResourceMessageException {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace jjournal.Exception {
         ///   Retorna a instância de ResourceManager armazenada em cache usada por essa classe.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("jjournal.Exception.ResourceMessageException", typeof(ResourceMessageException).Assembly);
@@ -51,7 +51,7 @@ namespace jjournal.Exception {
         ///   pesquisas de recursos que usam essa classe de recurso de tipo de alta segurança.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace jjournal.Exception {
         /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a &quot;Email can&apos;t be empty.&quot;.
         /// </summary>
-        internal static string EMAIL_EMPTY {
+        public static string EMAIL_EMPTY {
             get {
                 return ResourceManager.GetString("EMAIL_EMPTY", resourceCulture);
             }
@@ -72,7 +72,7 @@ namespace jjournal.Exception {
         /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a &quot;Name can&apos;t be empty.&quot;.
         /// </summary>
-        internal static string NAME_EMPTY {
+        public static string NAME_EMPTY {
             get {
                 return ResourceManager.GetString("NAME_EMPTY", resourceCulture);
             }
@@ -81,7 +81,7 @@ namespace jjournal.Exception {
         /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a &quot;Password should be greater than or equal to 6.&quot;.
         /// </summary>
-        internal static string PASSWORD_INVALID {
+        public static string PASSWORD_INVALID {
             get {
                 return ResourceManager.GetString("PASSWORD_INVALID", resourceCulture);
             }
