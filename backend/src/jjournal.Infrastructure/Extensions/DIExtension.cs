76dea0112d9d9c35c2b98889aeda0264f585258a
João Vidal <dev.joaovidal@gmail.com>
2024-09-21 22:22:21 -0300

    feat: validations and trouble setting resource as public

---
+++ backend/src/jjournal.Infrastructure/Extensions/DIExtension.cs
@@ -0,0 +1,22 @@
+﻿using jjournal.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace jjournal.Infrastructure.Extensions;
+
+public static class DIExtension
+{
+    public static void AddInfra(this IServiceCollection services, IConfiguration config)
+    {
+        AddDatabase(services, config);
+    }
+
+    private static void AddDatabase(IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(config.GetConnectionString("Default"));
+        });
+    }
+}
