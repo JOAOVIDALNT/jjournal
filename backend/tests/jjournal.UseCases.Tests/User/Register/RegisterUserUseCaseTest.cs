558444817d6c93ed54acb327758ffe83ac8b0397
João Vidal <dev.joaovidal@gmail.com>
2024-10-04 06:04:45 -0300

    feat: RegisterUser UseCase tests

--- backend/tests/jjournal.UseCases.Tests/User/Register/RegisterUserUseCaseTest.cs
+++ backend/tests/jjournal.UseCases.Tests/User/Register/RegisterUserUseCaseTest.cs
@@ -3,6 +3,11 @@ using jjournal.CommonTestUtilities.Repositories;
 using jjournal.CommonTestUtilities.Requests;
 using jjournal.CommonTestUtilities.Services;
 using jjournal.CommonTestUtilities.Validators;
+using jjournal.Domain.Interfaces.Repositories;
+using jjournal.Domain.Models.Entities;
+using jjournal.Exception;
+using jjournal.Exception.Base;
+using Moq;
 
 namespace jjournal.UseCases.Tests.User.Register
 {
@@ -13,26 +18,49 @@ namespace jjournal.UseCases.Tests.User.Register
         {
             var user = RegisterUserRequestBuilder.Build();
 
-            var useCase = CreateUseCase();
+            // INSTÂNCIAS DE MOCKS PARA TESTES SOBRE AÇÕES
+            var uowMock = new Mock<IUnitOfWork>(); 
+            var repoMock = new Mock<IUserRepository>();
+
+            var useCase = CreateMockUseCase(uowMock.Object, repoMock.Object);
 
             var result = await useCase.Execute(user);
             
             Assert.NotNull(result);
             Assert.Equal(result.Name, user.Name);
+            repoMock.Verify(repo => repo.CreateAsync(It.IsAny<Domain.Models.Entities.User>()), Times.Once);
+            uowMock.Verify(uow => uow.Commit(), Times.Once);
         }
 
-        private static RegisterUserUseCase CreateUseCase(string? email = null)
+        [Fact]
+        public async Task Error_User_Exists()
         {
-            var userRepository = new UserRepositoryBuilder();
+            var user = RegisterUserRequestBuilder.Build();
+
+            var uowMock = new Mock<IUnitOfWork>();
+            var repoMock = new Mock<IUserRepository>();
+
+            repoMock.Setup(repo => repo.UserExists(user.Email)).ReturnsAsync(true);
+
+            var useCase = CreateMockUseCase(uowMock.Object, repoMock.Object);
+
+            var act = async () => await useCase.Execute(user);
+
+            var exception = await Assert.ThrowsAsync<ErrorOnValidationException>(act);
+
+            Assert.Single(exception.ErrorMessages);
+            Assert.Equal(ResourceMessageException.EMAIL_EXISTS, exception.ErrorMessages.First());
+            repoMock.Verify(repo => repo.CreateAsync(It.IsAny<Domain.Models.Entities.User>()), Times.Never);
+            uowMock.Verify(uow => uow.Commit(), Times.Never);
+        }
+
+        private static RegisterUserUseCase CreateMockUseCase(IUnitOfWork uow, IUserRepository repo, string? email = null)
+        {
             var mapper = MapperBuilder.Build();
             var hasher = PasswordHasherBuilder.Build();
-            var uow = UnitOfWorkBuilder.Build();
             var validator = new RegisterUserValidatorBuilder();
 
-            if (!string.IsNullOrEmpty(email))
-                userRepository.UserExists(email);
-
-            return new RegisterUserUseCase(userRepository.Build(), validator.Build(userRepository.Build()), mapper, hasher, uow);
+            return new RegisterUserUseCase(repo, validator.Build(repo), mapper, hasher, uow);
         }
     }
 }
